3cde146d6fc204c7e35bee9b252958d4c99dc4de
吴子豪 <vortezwohl@proton.me>
2024-08-27 01:07:43 +0800

    hotfix: replace default metric to cosine

--- medusa/model/face_embedding.py
+++ medusa/model/face_embedding.py
@@ -6,7 +6,7 @@ from medusa.model import vggface2_VGG16_param52658_acc97
 COSINE = 'cosine'
 EUCLIDEAN = 'euclidean'
 EUCLIDEAN_L2 = 'euclidean_l2'
-DEFAULT_METRIC = EUCLIDEAN_L2
+DEFAULT_METRIC = COSINE
 
 THRESHOLDS = {
     'VGG-Face-Embedding': {
